5454ba05c126b65c27b013a7c0c8fc3ebbf4c593
Benjamin Henrion <zoobab@gmail.com>
2018-12-21 08:49:57 +0000

    stop on error

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
+
 WORKDIR="$PWD/release/firmware/STM32F103C8_BluePill"
 LOGFILE="$WORKDIR/build.log"
 rm -f $LOGFILE
 exec &> >(tee -a "$LOGFILE")
 docker build -t versaloon:latest .
 docker run -v $WORKDIR:/mnt versaloon:latest bash -c "sudo cp -v /home/versaloon/code/dongle/firmware/Projects/Versaloon/GCC/* /mnt"
-
