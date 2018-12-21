fb99e3451609873dc496d88619371b8b7e52ae87
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2018-12-21 22:05:25 +0100

    check the exitcode, stm32flash output is not well made

--- flash.sh
+++ flash.sh
@@ -2,3 +2,9 @@
 DEVICE="/dev/ttyUSB0"
 FILE="Versaloon-STM32F103C8_BluePill.bin"
 docker run --privileged -v $DEVICE:$DEVICE -v $PWD:/mnt zoobab/stm32flash-docker -w /mnt/$FILE -v -g 0x8000000 $DEVICE
+EXITCODE="$?"
+if [ $EXITCODE -eq 0 ]; then
+	echo "OK, flashing is successful!"
+else
+	echo "ERROR, there was a flashing error :-("
+fi
