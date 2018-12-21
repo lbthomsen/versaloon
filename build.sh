565976e35d9ded128ba18ebd6684c4e98950c1a2
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2018-12-21 20:34:14 +0100

    remove unneeded stuff from build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-
-WORKDIR="$PWD"
-LOGFILE="$WORKDIR/build.log"
-rm -f $LOGFILE
-exec &> >(tee -a "$LOGFILE")
-docker build -t versaloon:latest .
-docker run -v $WORKDIR:/mnt versaloon:latest bash -c "sudo cp -v /home/versaloon/code/*.bin /mnt"
+PROJECT="versaloon"
+docker build -t $PROJECT .
+docker run -v $PWD/bin:/mnt $PROJECT bash -c "sudo cp -v /home/versaloon/code/*.bin /mnt"
